014073fc8da27a79b186d3702dad83c557780a89
Justin Paul Palomiano Morada <justinpaulmorada969@gmail.com>
2025-06-12 01:29:31 +0800

    First Commit

---
+++ Activity1.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Code
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("\nDemonstrating C# Functions\n");
+
+        int num1 = 10;
+        int num2 = 5;
+        int sum = num1 + num2;
+
+        Console.WriteLine($"The sum of {num1} and {num2} is: {sum}");
+    }
+}
